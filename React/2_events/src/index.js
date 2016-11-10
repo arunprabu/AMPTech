e7aacf2b22c75fb6d68d733e02bf3d112b01d0ad
Arun Vijayarengan <arunprabu.v@hotmail.com>
2016-11-10 20:15:52 +0530

    First Day Training

---
+++ React/2_events/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class SearchBar extends React.Component {
+  render(){
+    return <h1>Hello</h1>;
+  }
+}
+
+ReactDOM.render(<SearchBar />, document.getElementById("root"));
